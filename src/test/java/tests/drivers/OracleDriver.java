a37be605091b07edc5660346d0bcaf96430683fc
Ivaylo Goranovski <ivaylo.goranovski@gmail.com>
2022-07-21 16:18:38 +0300

    add oracle interfaces

--- src/test/java/tests/drivers/OracleDriver.java
+++ src/test/java/tests/drivers/OracleDriver.java
@@ -2,6 +2,8 @@ package tests.drivers;
 
 import tests.interfaces.iOracleQueries;
 
+import java.sql.SQLException;
+
 public class OracleDriver extends DatabaseDriver implements iOracleQueries {
 
     public OracleDriver() {
@@ -25,6 +27,31 @@ public class OracleDriver extends DatabaseDriver implements iOracleQueries {
 
     @Override
     public void createTable(String tableName) {
+
+    }
+
+    @Override
+    public void insertRecord(String tableName, Integer id, String firstName, String lastName) {
+
+    }
+
+    @Override
+    public Integer countId(String tableName) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getLastname(String tableName) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public String getFirstname(String tableName) throws SQLException {
+        return null;
+    }
+
+    @Override
+    public void dropDb(String dbName) {
 
     }
 
